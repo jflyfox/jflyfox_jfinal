41f59b5eedfd21e823336b983dd4ed38f4739845
Administrator <Administrator@KKJL13-PC>
2017-01-18 17:28:25 +0800

    删除测试代码

--- src/main/java/com/jflyfox/util/cache/RedisCache.java
+++ src/main/java/com/jflyfox/util/cache/RedisCache.java
@@ -5,50 +5,9 @@ import java.util.Map;
 import java.util.Set;
 
 import com.jflyfox.jfinal.component.redis.JedisClient;
-import com.jflyfox.util.Config;
-import com.jflyfox.util.StrUtils;
-import com.jflyfox.util.cache.impl.MemoryCache;
-import com.jflyfox.util.cache.impl.MemorySerializeCache;
-import com.jflyfox.util.serializable.FSTSerializer;
-import com.jflyfox.util.serializable.SerializerManage;
 
 public class RedisCache implements Cache {
 
-	public static void main(String[] args) {
-		// 初始化Cache为fst序列化
-		SerializerManage.add("fst", new FSTSerializer());
-		
-		// 设置序列化工具
-		String defaultKey = Config.getStr("CACHE.SERIALIZER.DEFAULT");
-		defaultKey = StrUtils.isEmpty(defaultKey) ? "java" : defaultKey;
-		SerializerManage.setDefaultKey(defaultKey);
-
-		
-		// 设置缓存
-		CacheManager.setCache(new ICacheManager() {
-
-			public Cache getCache() {
-				String cacheName = Config.getStr("CACHE.NAME");
-				cacheName = StrUtils.isEmpty(cacheName) ? "MemorySerializeCache" : cacheName; 
-				
-				if ("MemorySerializeCache".equals(cacheName)) {
-					return new MemorySerializeCache();
-				} else if ("MemoryCache".equals(cacheName)) {
-					return new MemoryCache();
-				}  else if ("RedisCache".equals(cacheName)) {
-					return new RedisCache();
-				} else {
-					throw new RuntimeException("####init cache error!");
-				}
-			}
-		});
-		
-		System.out.println(11);
-		CacheManager.get("testaaa").add("testaaa", "1");
-		System.out.println(CacheManager.get("testaaa").get("testaaa"));
-		CacheManager.get("testaaa").remove("testaaa");
-		System.out.println(CacheManager.get("testaaa").get("testaaa"));
-	}
 	protected String name;
 	protected JedisClient client = null;
 
